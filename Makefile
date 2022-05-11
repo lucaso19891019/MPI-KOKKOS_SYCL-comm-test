09563723e71c3bdb2b7f369bb41adf4d4eab30e2
lucaso19891019 <79555555+lucaso19891019@users.noreply.github.com>
2022-05-11 13:06:12 -0500

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,38 @@
+KOKKOS_PATH = /grand/catalyst/world-shared/spatel/kokkos/source
+KOKKOS_DEVICES = "SYCL"
+KOKKOS_ARCH = "Ampere80"
+KOKKOS_CUDA_OPTIONS = "enable_lambda"
+KOKKOS_CXX_STANDARD=c++17
+
+default: waitall_ex
+
+
+
+USERFLAGS = -O3 -fsycl -fsycl-targets=nvptx64-nvidia-cuda-sycldevice -Wno-unknown-cuda-version
+USERFLAGS += -g -std=c++17 -I../   
+CXXFLAGS = $(USERFLAGS)
+LINKFLAGS = $(USERFLAGS)
+CXX = mpicxx
+
+LINK = $(CXX)
+
+DEPFLAGS = -M
+LIB =
+
+include $(KOKKOS_PATH)/Makefile.kokkos
+
+FOBJS = waitall_ex.o
+
+
+
+waitall_ex: $(FOBJS) $(KOKKOS_LINK_DEPENDS)
+	$(LINK) $(KOKKOS_LDFLAGS) $(LINKFLAGS) $(EXTRA_PATH) $(FOBJS) $(KOKKOS_LIBS) $(LIB) -o $@
+
+clean: kokkos-clean
+	rm -f *.o *.h *.tmp *.hpp *.a; cd $(builddir); rm -f *.o *.cuda *.host *.dat
+
+# Compilation rules
+
+%.o:%.cpp $(KOKKOS_CPP_DEPENDS)
+	$(CXX) $(KOKKOS_CPPFLAGS) $(KOKKOS_CXXFLAGS) $(CXXFLAGS) $(EXTRA_INC) -c $<
+
---
+++ waitall_ex.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#include <Kokkos_Core.hpp>
+#define myDeviceMemorySpace Kokkos::Experimental::SYCLDeviceUSMSpace
+#define myPolicy Kokkos::RangePolicy<myDeviceMemorySpace::execution_space>
+#define myViewInt Kokkos::View<int *, myDeviceMemorySpace>
+#define myMirrorViewInt Kokkos::View<int *, myDeviceMemorySpace>::HostMirror
+
+/**
+ * @brief Illustrates how to wait for the completion of multiple non-blocking
+ * operations.
+ * @details This program is meant to be run with even number of processes.
+ **/
+int main(int argc, char* argv[])
+{
+    MPI_Init(&argc, &argv);
+    // Get the number of processes and check only 3 processes are used
+    int size;
+	
+	
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if(size%2!=0)
+	{
+		printf("The number of ranks must be even.\n");
+		exit(-1);
+	}
+    // Get my rank
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    //CudaComm* cudacomm;
+
+
+        myViewInt bufferSend,bufferRecv;
+	myMirrorViewInt Send,Recv;
+	
+	bufferSend=myViewInt("bufferSend",1);
+	bufferRecv=myViewInt("bufferRecv",2);
+
+	Send=Kokkos::create_mirror_view(bufferSend);
+	Recv=Kokkos::create_mirror_view(bufferRecv);
+	
+	Send(1)=my_rank;
+	Kokkos::deep_copy(bufferSend,Send);
+
+	Kokkos::fence();
+	
+    if(my_rank <size/2)
+    {
+	// The MPI process sends the message.
+
+        MPI_Request requests[2];
+        printf("MPI process %d sends the value %d.\n", my_rank, Send(0));
+        MPI_Isend(bufferSend.data(), 1, MPI_INT, my_rank+size/2, 0, MPI_COMM_WORLD, &requests[0]);
+	MPI_Isend(bufferSend.data(), 1, MPI_INT, size-1-my_rank, 0, MPI_COMM_WORLD, &requests[1]);
+
+// Wait for both routines to complete
+        MPI_Waitall(2, requests, MPI_STATUSES_IGNORE);
+        printf("Process %d: both messages have been sent.\n", my_rank);
+
+// The MPI processes receive the message.       	
+	MPI_Status status[2];
+        MPI_Recv(bufferRecv.data(), 1, MPI_INT, my_rank+size/2, 0, MPI_COMM_WORLD, &status[0]);
+	MPI_Recv(bufferRecv.data()+1, 1, MPI_INT, size-1-my_rank, 0, MPI_COMM_WORLD, &status[1]);
+
+	Kokkos::deep_copy(Recv,bufferRecv);
+        printf("Process %d received value %d from %d and value %d from %d.\n", my_rank, Recv(0),my_rank+size/2,Recv(1),size-1-my_rank);
+
+        
+    }
+    else
+    {
+
+// The MPI process sends the message.
+
+        MPI_Request requests[2];
+        printf("MPI process %d sends the value %d.\n", my_rank, Send(0));
+        MPI_Isend(bufferSend.data(), 1, MPI_INT, my_rank-size/2, 0, MPI_COMM_WORLD, &requests[0]);
+	MPI_Isend(bufferSend.data(), 1, MPI_INT, size-1-my_rank, 0, MPI_COMM_WORLD, &requests[1]);
+
+// Wait for both routines to complete
+        MPI_Waitall(2, requests, MPI_STATUSES_IGNORE);
+        printf("Process %d: both messages have been sent.\n", my_rank);
+
+        // The MPI processes receive the message.       	
+	MPI_Status status[2];
+        MPI_Recv(bufferRecv.data(), 1, MPI_INT, my_rank-size/2, 0, MPI_COMM_WORLD, &status[0]);
+	MPI_Recv(bufferRecv.data()+1, 1, MPI_INT, size-1-my_rank, 0, MPI_COMM_WORLD, &status[1]);
+
+	Kokkos::deep_copy(Recv,bufferRecv);
+        printf("Process %d received value %d from %d and value %d from %d.\n", my_rank, Recv(0),my_rank-size/2,Recv(1),size-1-my_rank);
+    }
+
+	
+
+
+    MPI_Finalize();
+    return EXIT_SUCCESS;
+}
